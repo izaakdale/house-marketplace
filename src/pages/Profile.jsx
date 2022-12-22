198e7a63c9ff515d9b24a513f5a2f827e2688cd1
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-12-22 15:53:19 +0000

    creating profile and logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import { getAuth } from 'firebase/auth'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
 
-    const [user, setUser] = useState(null)
-
     const auth = getAuth()
-    useEffect(() => {
-        setUser(auth.currentUser)
-    }, [setUser])
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
+    })
 
+    const {name, email} = formData
 
-    return user ? <h1>{user.displayName}</h1> : 'not logged in'
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        auth.signOut()
+        navigate('/')
+    }
+
+    return (
+        <div className='profile'>
+            <header className="profileHeader">
+                <p className='pageHeader'>My Profile</p>
+                <button type='button' className='logOut' onClick={onLogout}>
+                    Logout
+                </button>
+            </header>
+        </div>
+    )
 }
 
 export default Profile
