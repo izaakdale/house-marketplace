d2af43dc3a816f9dac2966063218ae6dccf1e6a8
izaakdale <64863677+izaakdale@users.noreply.github.com>
2023-01-08 22:49:31 +0000

    ForgotPassword page completed

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { FaArrowAltCircleRight } from 'react-icons/fa'
+import { toast } from 'react-toastify'
 
 function ForgotPassword() {
+  const [email, setEmail] = useState('')
+
+  const onChange = (e) => setEmail(e.target.value)
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success('email sent!')
+    } catch (error) {
+      console.log(error);
+      toast.error('hmmm...')
+    }
+  }
+
   return (
-    <div>ForgotPassword</div>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input type="email"  id="email" placeholder='email...' className='emailInput' onChange={onChange}/>
+        <Link className='forgotPasswordLink' to='/sign-in'>sign-in</Link>
+
+        <div className="signInBar">
+          <div className="signInText">Send Reset Link</div>
+          <button className='signInButton'>
+              <FaArrowAltCircleRight size={100} fill='#ffffff' width='34px' height='34px'/>
+          </button>
+        </div>
+        </form>
+      </main>
+    </div>
   )
+
 }
 
 export default ForgotPassword
